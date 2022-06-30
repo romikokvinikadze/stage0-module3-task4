37ab486e8aa66b95f5bb9398d58666f09f30a38a
romikokvinikadze <r.kvinikadze11@gmail.com>
2022-06-30 16:31:59 +0400

    primitives

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double a = 1.0;
+        double b = 2.0;
+
+        double formula = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
